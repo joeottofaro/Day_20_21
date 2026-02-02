aab4d611143ba2b1f785536acc16f62e792f336b
joeottofaro <joseph.ottofaro@gmail.com>
2026-02-02 12:17:11 +0000

    added controls

--- main.py
+++ main.py
@@ -10,6 +10,16 @@ screen.tracer(0)
 
 snake = Snake()
 
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.up, "w")
+screen.onkey(snake.down, "s")
+screen.onkey(snake.left, "a")
+screen.onkey(snake.right, "d")
+
 game_is_on = True
 while game_is_on:
     screen.update()
