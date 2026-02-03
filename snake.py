530ad6483439866494ccf09192c5453e2a0f941a
joeottofaro <joseph.ottofaro@gmail.com>
2026-02-03 12:54:19 +0000

    I am speed

--- snake.py
+++ snake.py
@@ -13,6 +13,7 @@ class Snake:
         self.segments = []
         self.create_snake()
         self.head = self.segments[0]
+        self.speed = 3
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
@@ -28,6 +29,9 @@ class Snake:
     def extend(self):
         self.add_segment(self.segments[-1].position())
 
+    def increase_speed(self):
+        self.speed += 0.5
+
     def move(self):
         # To move snake and set the x/y cor to the 2nd to last segment in the segments list
         for seg_num in range(len(self.segments) - 1, 0, -1):
@@ -35,6 +39,7 @@ class Snake:
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
         self.head.forward(MOVE_DISTANCE)
+        self.head.speed(self.speed)
 
     def up(self):
         if self.head.heading() != DOWN:
